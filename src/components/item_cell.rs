102e4950d58df3af8581070d4c4e7e3a5548e830
Denis Denisov <denis.denisov8701@gmail.com>
2024-09-01 15:57:45 +0200

    make same size for cells

--- src/components/item_cell.rs
+++ src/components/item_cell.rs
@@ -14,19 +14,26 @@ pub fn ProductItem<'a>(product: &'a Item) -> impl IntoView {
 
     view! {
         <div class="product-item">
-            <img src=product.image_url alt=product.name class="product-image" />
-            <p>{product.name}</p>
-            <div class="descr">{product.description}</div>
-            <p>{format!("Price: {}€ ", product.price)}</p>
-            <div class="product-status">
-                {match product.status {
-                    ItemStatus::Vendido => view! { <div class="sold">"Vendido"</div> },
-                    ItemStatus::Disponible => view! { <div class="available">"Disponible"</div> },
-                    ItemStatus::Reservado => view! { <div class="reserved">"Reservado"</div> },
-                }}
+            <div class="product-image">
+                <a href=product.image_url target="_blank">
+                    <img src=product.image_url alt=product.name />
+                </a>
             </div>
-
-            {whatsapp_button()}
+            <div>
+                <p>{product.name}</p>
+                <div class="descr">{product.description}</div>
+                <p>{format!("Price: {}€ ", product.price)}</p>
+            </div>
+            <div>
+                <div class="product-status">
+                    {match product.status {
+                        ItemStatus::Vendido => view! { <div class="sold">"Vendido"</div> },
+                        ItemStatus::Disponible => view! { <div class="available">"Disponible"</div> },
+                        ItemStatus::Reservado => view! { <div class="reserved">"Reservado"</div> },
+                    }}
+                </div>
+                {whatsapp_button()}
+            </div>
         </div>
     }
 }
